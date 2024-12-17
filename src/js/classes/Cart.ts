a308803009698a878afbb126cb8c3ac8c0583486
Artem Safin <artemsafin.webdev@gmail.com>
2024-12-17 05:40:34 +0300

    Добавленное

---
+++ src/js/classes/Cart.ts
@@ -0,0 +1,19 @@
+type CartSchema = {};
+
+class Cart {
+  constructor() {}
+
+  private load = () => {};
+
+  private save = () => {};
+
+  public add = () => {};
+
+  public remove = (id: string) => {};
+
+  public increment = (id: string) => {};
+
+  public decrement = (id: string) => {};
+
+  public clear = () => {};
+}
